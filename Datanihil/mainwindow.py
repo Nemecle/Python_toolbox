5ec5a6fd68eea571e74744de05d058296aa9c948
Nemecle <nemecle@protonmail.com>
2016-02-24 00:13:12 +0100

    basic window for dayanihil

--- Datanihil/mainwindow.py
+++ Datanihil/mainwindow.py
@@ -8,11 +8,31 @@ the support of my company (for a personal use)
 
 """
 
+import Tkinter as Tk
+
+class Interface(Tk.Frame):
+    """
+    definition of the main window interface (self explainatory huh?)
+
+    """
+
+    def __init__(self, window):
+        Tk.Frame.__init__(self, window, width=600, height=400)
+        self.root = window
+        self.pack(fill=Tk.BOTH)
+
+
+        self.root.mainloop()
+
+
 def main():
     """
     main function
 
     """
+
+    window = Tk.Tk()
+    app = Interface(window)
     pass
 
 
