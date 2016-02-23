742044f48393d378967c71c63ce2adda27fcdb54
Nemecle <nemecle@protonmail.com>
2016-02-23 23:51:16 +0100

    added dedicated folder for note app test

---
+++ Datanihil/mainwindow.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# coding: latin-1
+
+"""
+main window for the Datanihil app test
+This app aims to provide an easy way to write down procedures for
+the support of my company (for a personal use)
+
+"""
+
+def main():
+    """
+    main function
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
