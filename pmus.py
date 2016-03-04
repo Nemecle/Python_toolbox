ca97f1086d0384e69cbfe2e8242a4fa05a1b9ab6
Nemecle <nemecle@protonmail.com>
2016-03-04 21:22:49 +0100

    working on music analysis, can't import aubio correctly for now

--- pmus.py
+++ pmus.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
-import aubio
+
+import aubio.slicing
 from os import listdir
 from os.path import isfile, join
 import os 
+import random
 
+# sudo mount -t vboxsf music /mnt/music
 
 def main():
     """
@@ -13,12 +16,14 @@ def main():
     """
     folderpath = "/mnt/music"
 
-    shpfiles = []
+    musics = []
     for dirpath, subdirs, files in os.walk(folderpath):
         for x in files:
             if x.endswith(".mp3"):
-                shpfiles.append(os.path.join(dirpath, x))
-    print str(shpfiles)
+                musics.append(os.path.join(dirpath, x))
+    #print str(shpfiles)
+    print len(musics)
+    print random.choice(musics)
 
 if __name__ == '__main__':
     main()
